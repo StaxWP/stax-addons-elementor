79c9c93dd1eaabe74bd84c600b6fba3f956444fc
George Tudor <georgebitq@gmail.com>
2020-01-29 13:33:27 +0200

    fix gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
-var path = require('path'),
+const path = require('path'),
     gulp = require('gulp'),
     sass = require('gulp-sass'),
     rename = require('gulp-rename'),
-    csso = require('gulp-csso');
+    csso = require('gulp-csso'),
+    notify = require('gulp-notify');
 
-gulp.task('styles', function () {
-    gulp.src('./widgets/**/*.scss')
+gulp.task('build:styles', () => {
+    return gulp.src('./widgets/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest(path.join(__dirname, 'widgets')))
         .pipe(csso())
@@ -14,9 +15,10 @@ gulp.task('styles', function () {
                 suffix: ".min"
             })
         )
-        .pipe(gulp.dest(path.join(__dirname, 'widgets')));
+        .pipe(gulp.dest(path.join(__dirname, 'widgets')))
+        .pipe(notify({message: 'CSS Compiled'}));
 });
 
-gulp.task('watch', function () {
-    gulp.watch('./widgets/**/*.scss', gulp.series(['styles']));
+gulp.task('watch:styles', () => {
+    gulp.watch('./widgets/**/*.scss', gulp.series(['build:styles']));
 });
