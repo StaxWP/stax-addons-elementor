a5c9014598d74b0d4801dbed4b167c9853b520ce
Robert Neacsu <robert.neacsu@seventhqueen.com>
2020-06-24 13:37:02 +0300

    admin toggle widhets restyled.

--- core/admin/pages/templates/widgets.php
+++ core/admin/pages/templates/widgets.php
@@ -1,22 +1,26 @@
 <h2 class="ste-my-0 ste-leading-none ste-text-2xl ste-text-gray-700 ste-font-bold ste-tracking-wide">
-	<?php _e( 'Widgets', 'stax-addons-for-elementor' ); ?>
+    <?php _e( 'Widgets', 'stax-addons-for-elementor' ); ?>
 </h2>
 
 <div class="ste-mt-5">
     <form action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>" method="POST">
         <div class="ste-flex ste-flex-wrap ste--mx-2">
-			<?php foreach ( $widgets as $key => $widget ) : ?>
+            <?php foreach ( $widgets as $key => $widget ) : ?>
                 <div class="ste-my-2 ste-w-full md:ste-w-1/2 lg:ste-w-1/3 xl:ste-w-1/4">
                     <div class="ste-mx-2">
                         <label for="module-label-<?php echo $key ?>"
-                               class="ste-flex ste-justify-between ste-items-center ste-border ste-border-solid ste-border-gray-300 ste-rounded ste-bg-gray-100 ste-p-4 hover:ste-border-gray-400">
-                            <span class="ste-font-medium ste-text-gray-600"><?php echo $widget['name'] ?></span>
-                            <input type="checkbox" name="<?php echo esc_attr( $widget['slug'] ) ?>"
-                                   id="module-label-<?php echo $key ?>" <?php checked( $widget['status'] ) ?>>
+                               class="ste-flex ste-justify-between ste-items-center ste-border-2 ste-border-solid ste-border-gray-300 ste-rounded ste-bg-gray-100 ste-p-4 hover:ste-border-gray-400">
+                            <span class="ste-font-small ste-text-gray-600 ste-text-lg"><?php echo $widget['name'] ?></span>
+                            <div class="ste-relative">
+                                <input type="checkbox" name="<?php echo esc_attr( $widget['slug'] ) ?>"
+                                       id="module-label-<?php echo $key ?>" class="ste-toggle-input" <?php checked( $widget['status'] ) ?>>
+                                <div class="ste-toggle-line ste-w-10 ste-h-4 ste-bg-gray-400 ste-rounded-full ste-shadow-inner"></div>
+                                <div class="ste-toggle-dot ste-absolute ste-w-6 ste-h-6 ste-bg-white ste-rounded-full ste-shadow ste-inset-y-0 ste-left-0"></div>
+                            </div>
                         </label>
                     </div>
                 </div>
-			<?php endforeach; ?>
+            <?php endforeach; ?>
         </div>
         <input type="hidden" name="action" value="stax_widget_activation">
 
