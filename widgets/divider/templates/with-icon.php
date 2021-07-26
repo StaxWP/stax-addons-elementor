1f599f0a2f144f37b366429a819f9f5c64f39d15
George Tudor <george@seventhqueen.com>
2021-07-26 11:28:40 +0300

    divider layour options

--- widgets/divider/templates/with-icon.php
+++ widgets/divider/templates/with-icon.php
@@ -1,9 +1,9 @@
 <div class="stx-divider-with-icon">
 	<div class="stx-m-line">
 		<div class="stx-m-inner-line"></div>
-		<?php if ( ! empty( $settings['separator_icon'] ) ) : ?>
+		<?php if ( ! empty( $settings['icon'] ) ) : ?>
 			<span class="stx-m-separator-icon">
-				<?php \Elementor\Icons_Manager::render_icon( $settings['separator_icon'], [ 'aria-hidden' => 'true' ] ); ?>
+				<?php \Elementor\Icons_Manager::render_icon( $settings['icon'], [ 'aria-hidden' => 'true' ] ); ?>
 			</span>
 		<?php endif; ?>
 		<div class="stx-m-inner-line"></div>
