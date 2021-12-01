dac273e78c28d1e279f3836214deeb63397c2511
George Tudor <georgebitq@gmail.com>
2021-12-01 12:07:36 +0200

    update npm, tailwind & update admin ui

--- loader.php
+++ loader.php
@@ -4,11 +4,11 @@
  * Description: Beautiful & Fast add-ons for Elementor. Enhance your site building experience with Stax Elementor Addons
  * Plugin URI: https://staxwp.com
  * Author: StaxWP
- * Version: 1.4.0
+ * Version: 1.4.1
  * Author URI: https://staxwp.com
  *
- * Elementor tested up to: 3.2.1
- * Elementor Pro tested up to: 3.2.1
+ * Elementor tested up to: 3.4.8
+ * Elementor Pro tested up to: 3.5.1
  *
  * Text Domain: stax-addons-for-elementor
  * Domain Path: /languages
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'STAX_EL_VERSION', '1.4.0' );
+define( 'STAX_EL_VERSION', '1.4.1' );
 define( 'STAX_EL_DOMAIN', 'stax-addons-for-elementor' );
 define( 'STAX_EL_HOOK_PREFIX', 'stax_el_' );
 define( 'STAX_EL_SLUG_PREFIX', 'stax-elementor-' );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,37 @@
 module.exports = {
   prefix: "ste-",
-  theme: {},
+  purge: {
+    enabled: true,
+    content: ["./core/admin/*.php", "./core/admin/pages/**/*.php"],
+  },
+  darkMode: false,
+  theme: {
+    container: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+    },
+    extend: {
+      colors: {
+        ash: {
+          100: "#FCFCFC",
+          200: "#F8F8F8",
+          300: "#F4F4F4",
+          400: "#EBEBEB",
+          500: "#E3E3E3",
+          600: "#CCCCCC",
+          700: "#888888",
+          800: "#666666",
+          900: "#444444",
+        },
+      },
+    },
+  },
   variants: {},
+  plugins: [],
   corePlugins: {
     preflight: false,
   },
